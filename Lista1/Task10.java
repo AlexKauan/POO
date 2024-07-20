1a3f3f04a90e586047a34e56b6381ca8f6367dbd
AlexKauan <alexkauan.dev@gmail.com>
2024-07-20 04:29:05 -0300

    correção de algumas tasks

--- Lista1/Task10.java
+++ Lista1/Task10.java
@@ -2,22 +2,22 @@ public class Task10 {
     public static void main(String[] args){
 
 
-        if(3<5 || 4>6){
-            System.out.println("Entrou");
-        }else{
-            System.out.println("não entrou");
-        }
-        int i = 4;
-        switch (i){
-            case 1:
-            case 2:
-            case 3:
-                System.out.println("Entrou aqui");
-                break;
+    //     if(3<5 || 4>6){
+    //         System.out.println("Entrou");
+    //     }else{
+    //         System.out.println("não entrou");
+    //     }
+    //     int i = 4;
+    //     switch (i){
+    //         case 1:
+    //         case 2:
+    //         case 3:
+    //             System.out.println("Entrou aqui");
+    //             break;
 
-            case 4:
-                System.out.println("Entrou aqui 4");
-                break;
-        }
-    }
+    //         case 4:
+    //             System.out.println("Entrou aqui 4");
+    //             break;
+    //     }
+ }
 }
--- Lista1/Task11.java
+++ Lista1/Task11.java
@@ -2,33 +2,31 @@ import java.util.Scanner;
 
 public class Task11 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Digite as coordenadas do primeiro ponto (x1 y1): ");
-        double x1 = scanner.nextDouble();
-        double y1 = scanner.nextDouble();
-
-        System.out.print("Digite as coordenadas do segundo ponto (x2 y2): ");
-        double x2 = scanner.nextDouble();
-        double y2 = scanner.nextDouble();
-
-        if (y2 > y1) {
-            System.out.println("O segundo ponto está acima do primeiro ponto.");
-        } else if (y2 < y1) {
-            System.out.println("O segundo ponto está abaixo do primeiro ponto.");
-        } else {
-            System.out.println("O segundo ponto está na mesma altura do primeiro ponto.");
+        try (Scanner log = new Scanner(System.in)) {
+            System.out.print("Digite as coordenadas do primeiro ponto (x1 y1): ");
+            double x1 = log.nextDouble();
+            double y1 = log.nextDouble();
+            
+            System.out.print("Digite as coordenadas do segundo ponto (x2 y2): ");
+            double x2 = log.nextDouble();
+            double y2 = log.nextDouble();
+            
+            if (y2 > y1) {
+                System.out.println("O segundo ponto está acima do primeiro ponto.");
+            } else if (y2 < y1) {
+                System.out.println("O segundo ponto está abaixo do primeiro ponto.");
+            } else {
+                System.out.println("O segundo ponto está na mesma altura do primeiro ponto.");
+            }
+            
+            if (x2 > x1) {
+                System.out.println("O segundo ponto está à direita do primeiro ponto.");
+            } else if (x2 < x1) {
+                System.out.println("O segundo ponto está à esquerda do primeiro ponto.");
+            } else {
+                System.out.println("O segundo ponto está na mesma linha vertical do primeiro ponto.");
+            }
         }
-
-        if (x2 > x1) {
-            System.out.println("O segundo ponto está à direita do primeiro ponto.");
-        } else if (x2 < x1) {
-            System.out.println("O segundo ponto está à esquerda do primeiro ponto.");
-        } else {
-            System.out.println("O segundo ponto está na mesma linha vertical do primeiro ponto.");
-        }
-
-        scanner.close();
     }
 }
 
