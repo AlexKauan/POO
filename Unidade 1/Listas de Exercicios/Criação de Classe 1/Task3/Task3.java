a7618556b85bc8b41199874d854a0ab5ae1b0e5d
Alex Kauan <alexkauan.dev@gmail.com>
2026-04-08 09:01:16 -0300

    remove PDF

--- Unidade 1/Listas de Exercicios/Criação de Classe 1/Task3/Task3.java
+++
@@ -1,9 +0,0 @@
-public class Task3 {
-    public static void main(String[] args) {
-        Aluno a = new Aluno("2026001", "Maria", 6.0, 5.0, 7.0);
-        System.out.println("Aluno: " + a.getMatricula() + " - " + a.getNome());
-        double mp = a.media();
-        System.out.println("MP: " + mp);
-        System.out.println("EF necessária: " + a.provaFinal());
-    }
-}
